51987f86ecc4db9b4f2f79e9135ad97fa9645a36
Manan Kotak <manankotak99@gmail.com>
2021-05-17 11:06:55 +0530

    first commit

---
+++ ui.R
@@ -0,0 +1,46 @@
+## ui.R
+library(shiny)
+library(shinydashboard)
+library(recommenderlab)
+library(data.table)
+install.packages('devtools')
+library(devtools)
+
+slam_url <- "https://cran.r-project.org/src/contrib/Archive/slam/slam_0.1-37.tar.gz"
+install_url(slam_url)
+devtools::install_github("stefanwilhelm/ShinyRatingInput", force = TRUE)
+library(shinyjs)
+library(ShinyRatingInput)
+source('functions/helpers.R')
+
+shinyUI(
+    dashboardPage(
+          skin = "blue",
+          dashboardHeader(title = "Book Recommender"),
+          
+          dashboardSidebar(disable = TRUE),
+
+          dashboardBody(includeCSS("css/books.css"),
+              fluidRow(
+                  box(width = 12, title = "Step 1: Rate as many books as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                      div(class = "rateitems",
+                          uiOutput('ratings')
+                      )
+                  )
+                ),
+              fluidRow(
+                  useShinyjs(),
+                  box(
+                    width = 12, status = "info", solidHeader = TRUE,
+                    title = "Step 2: Discover books you might like",
+                    br(),
+                    withBusyIndicatorUI(
+                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                    ),
+                    br(),
+                    tableOutput("results")
+                  )
+               )
+          )
+    )
+) 
